3bf0cb0fea43dc4f09f8bd91c4b52f797258451f
root <root@ip-10-0-1-91.ec2.internal>
2025-09-21 08:36:16 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# Create a simple Python web app
+cat <<EOF > app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Jenkins + Docker!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
